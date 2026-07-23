6a94b118eefd26d49595a2bd64bd7e0e89dc1a3b
akakabrian <akakabrian19@gmail.com>
2026-07-22 18:11:58 -1000

    Import girth-four replacement theorem

--- WOW144.lean
+++ WOW144.lean
@@ -8,6 +8,7 @@ import WOW144.NearestCycle
 import WOW144.Geodesic
 import WOW144.GeodesicExtension
 import WOW144.GeodesicNeighbor
+import WOW144.GirthFourReplacement
 import WOW144.SmallGirth
 import WOW144.CenterDepthOne
 import WOW144.MetricBounds
